b9d362f4ad6053a08e594a40930a083c91682ffd
hansj <hansj2041@gmail.com>
2023-01-11 11:57:34 +0900

    주석 제거

--- 03_DataStructure/01_Array_N_List/02_Average.js
+++ 03_DataStructure/01_Array_N_List/02_Average.js
@@ -1,16 +1,4 @@
 // 1546
-/* 슈도코드
-    과목 개수, 시험 성적 입력
-    변수 M에 최대값 구해서 저장
-    변수 SUM 선언
-    
-    FOR(과목 개수) {
-        과목점수 = 현재과목점수 / M * 100
-        변수 SUM에 다시 계산한 과목점수 더하기
-    }
-
-    평균(SUM / 과목 개수) 출력
-*/
 const [n, input] = require('fs').readFileSync(`/dev/stdin`).toString().trim().split(`\n`);
 const inputArr = input.trim().split(` `);
 
